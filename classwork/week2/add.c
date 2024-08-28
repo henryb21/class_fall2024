b960e4067df6acd000fdfba911ca31593645c13e
henry.ball <m77z631@csci112vm.cs.montana.edu>
2024-08-28 12:35:33 -0600

    initial commit

---
+++ classwork/week2/add.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+	int a;
+	int b;
+	a = 1;
+	b = 2;
+
+
---
+++ classwork/week2/mon/hello_world.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+	
+	// prints "Hello, world!"
+	printf("Hello, world!\n");
+
+	// exit with no errors
+	return(0);
+}
+
