3d5b67d23779594bd7e7b3c12b5ca820bd65b253
henry.ball <m77z631@csci112vm.cs.montana.edu>
2024-08-30 13:46:06 -0600

    finished lab1

--- labs/lab1/lab1.c
+++ labs/lab1/lab1.c
@@ -1,5 +1,20 @@
-#include<stdio.c>
+#include<stdio.h>
+#define minuteConversion 60 
+int main() {
+	double hours;
+	double minutes;
+	double totalTime;
+	double currentTemp;
 
-int main(){
+	printf("Hours and minutes since power outage? (e.g., 2 30 for 2 hours, 30 minutes)");
+	scanf("%lf %lf", &hours, &minutes);
+
+	totalTime = (hours + (minutes / minuteConversion));
+	currentTemp = (((3 * (totalTime * totalTime))/(totalTime + 1)) - 10);
 	
+	//printf("Entered hours: %lf\n", hours);
+	//printf("Entered minutes: %lf\n", minutes);
+	//printf("Calculuted total time: %lf\n", totalTime);
+	printf("Estimated current temperature: %.2lf degrees Celsius\n", currentTemp);
+		
 }
